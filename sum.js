1cc90bad8facd452e78251d012d388ed615461e8
Henrique Pires <henrique.p.espindola@gmail.com>
2020-04-03 07:07:34 -0300

    added changes at sum.js

--- sum.js
+++ sum.js
@@ -3,11 +3,6 @@ var bodyElement = document.querySelector('body');
 bodyElement.setAttribute('class', 'corpo');
 
 
-var divElement = document.createElement('div');
-divElement.setAttribute('class', 'firstBoxOnPage');
-
-
-bodyElement.appendChild(divElement);
 
 
 
@@ -15,7 +10,3 @@ bodyElement.appendChild(divElement);
 
 
 
-
-const numbers = [1, 2, 3, 4, 5, 6];
-const plusOneNumbers = numbers.map(n => n + 1);
-console.log(plusOneNumbers);
