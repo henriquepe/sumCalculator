a7b5001d69fe0cfe9e402b886b63667c27fbd005
Henrique Pires <henrique.p.espindola@gmail.com>
2020-04-04 13:39:10 -0300

    was added an update for the js file

--- js/sum.js
+++ js/sum.js
@@ -1,21 +1,29 @@
- 
-  //primeiro valor do input
-  
+
   
 
-  //segundo valor do input
+  
   
   
 
   var buttonResult = document.querySelector('.result');
 
+
   buttonResult.addEventListener('click',function() 
   {
-    //busquei o primeiro valor a ser digitado na DOM
+    //buscando o input resultado na DOM
+    var inputResult = document.querySelector('.sumResult');
+
+    //resetando o input do resultado antes de incluir o novo valor.
+    inputResult.textContent = "";
+
+
+    /*primeiro valor do input
+    busquei o primeiro valor a ser digitado na DOM*/
     var firstNumber = document.querySelector('.firstValue');
     //busquei o valor digitado no campo e passei para float
     var firstNumberValue = parseFloat(firstNumber.value);
 
+    //segundo valor do input
     //busquei o segundo valor a ser digitado na DOM
     var secondNumber = document.querySelector('.secondValue');
     //busquei o valor digitado no campo e passei para float
@@ -24,33 +32,29 @@
     //realizei a soma dos dois valores digitados 
     var sumResult = (firstNumberValue + secondNumberValue);
 
-    //busquei o input que saira o resultado
-    var inputResult = document.querySelector('.sumResult');
-    inputResult.setAttribute('color', 'black');
-    var inputResultText = document.createTextNode(sumResult);
-    inputResult.appendChild(inputResultText);
+
+    //verifica se o valor nao eh um numero ou vazio.
+    if(isNaN(sumResult)){
+      alert("Favor digite valores escalares...");
+    }
+    else{
+      //set text content in result input 
+      var inputResultText = document.createTextNode(sumResult);
+      inputResult.appendChild(inputResultText);
+    }
+
+    
+  
+    //show result in c...
     console.log(inputResultText);
 
-
-    
-
-    
-
-  
-
-
-    alert(sumResult);
-    // var inputResultText = sumResult.value;
-
-    // inputResult.appendChild(inputResultText);
-    
-    
-    
-
+    //resetando os inputs de valores
+    firstNumber.value = "";
+    secondNumber.value = "";
   })
 
   
-
+  
 
 
 
